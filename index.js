856822259e304d2920903d3435b47d1312d0f939
artem1925 <157005541+artem1925@users.noreply.github.com>
2024-11-13 14:25:15 +0000

    Deploying to gh-pages from @ artem1925/goit-js-hw-12@ea6bd62b58a487c6e4ee35672ea8fb8efa1299d7 🚀

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as g,S as h,i as l}from"./assets/vendor-D73Uttp0.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const y="https://pixabay.com/api/",L="46841046-91a9933b60f500c097f960e29";let d=1;const v=15;async function b(s){const i={key:L,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:v,page:d};try{const o=await g.get(y,{params:i});if(o.status!==200)throw new Error("Error:${response.status}");return o.data}catch(o){console.error("Помилка отримання зображень",o)}}function w(){d=1}function S(){d+=1}const $=document.querySelector(".list-img"),I=new h(".img-link",{captions:!0,captionsData:"alt",captionDelay:250});function P(s){const i=s.map(({largeImageURL:o,webformatURL:n,tags:e,likes:t,views:r,comments:f,downloads:p})=>`<li class="list-img-item">
+      <a href="${o}" class="img-link">
+      <img src="${n}" alt="${e}" width="100%"/>
+       <div class="image-info">
+        <div class="text-info-item">
+          <h2 class="title">Likes</h2>
+          <p class="data-title">${t}</p>
+        </div>
+        <div class="text-info-item"
+          <h2 class="title">Views</h2>
+          <p class="data-title">${r}</p>
+        </div>
+        <div class="text-info-item">
+          <h2 class="title">Comments</h2>
+          <p class="data-title">${f}</p>
+        </div>
+        <div class="text-info-item">
+          <h2 class="title">Downloads</h2>
+          <p class="data-title">${p}</p>
+        </div>
+      </div>
+        </a>
+    </li> `).join("");$.insertAdjacentHTML("beforeend",i),I.refresh()}const u=document.querySelector(".form-img"),q=document.querySelector(".input-img"),x=document.querySelector(".list-img"),a=document.querySelector(".loader"),c=document.querySelector(".btn-load-more");c.classList.add("hidden");u.addEventListener("submit",E);c.addEventListener("click",C);function E(s){s.preventDefault(),u.reset();const i=q.value.trim();if(a.classList.remove("hidden"),c.classList.add("hidden"),x.innerHTML="",w(),i==="")return a.classList.add("hidden"),l.warning({position:"topCenter",title:"Поле не може бути пустим!",message:"Будь ласка,введіть свій запит!",backgroundColor:"#ef3040"});m()}async function m(){try{const s=await b(inputValue);a.classList.add("hidden"),s.hits.length===0&&l.warning({position:"topCenter",title:"Результатів не знайдено!",message:"На жаль,за вашим запитом не знайдено зображень.Спробуйте ще раз!",backgroundColor:"#ef3040"}),P(s.hits),s.totalHits>page*perPage?c.classList.remove("hidden"):l.info({position:"topCenter",title:"УПС!",message:"Нам дуже шкода,але за вашим запитом більше зображень не знайдено!"})}catch(s){console.error("Помилка отримання і відображення зображень",s)}}function C(){S(),a.classList.remove("hidden"),m()}
 //# sourceMappingURL=index.js.map
